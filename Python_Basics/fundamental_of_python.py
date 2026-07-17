ba178427668266d2cf6a7d574fb534a20f534a80
Anup Kumar Mishra <anupkrmi11122006@gmail.com>
2026-07-17 16:40:19 +0000

    Fundamental of Python

---
+++ Python_Basics/fundamental_of_python.py
@@ -0,0 +1,101 @@
+name = "Anup Kumar Mishra"
+age = 19
+num1 = 10.99
+num2 = -20
+num3 = 20 + 30.4
+num4 = num2 * num1
+check1 = True
+check2 = False
+
+print("My name is", name, "and I am", age, "years old.")
+
+print("Lets try to change the value of age variable.")
+age = 20
+print("My name is", name, "and I am", age, "years old.")
+
+print("Lets try to change the value of name variable.")
+name = "Anup Kumar Mishra"
+print("My name is", name, "and I am", age, "years old.")
+
+print("Lets try to calculate area of rectangle.")
+length = 10
+breadth = 5
+area = length * breadth
+print("The area of rectangle with length", length, "and breadth", breadth, "is:", area)
+
+print("Lets try to learn about data types in Python.")
+print("The data type of name is:", type(name))
+print("The data type of age is:", type(age))
+print("The data type of length is:", type(length))
+print("The data type of breadth is:", type(breadth))
+print("The data type of area is:", type(area))
+print("The data type of num1 is:", type(num1))
+print("The data type of num2 is:", type(num2))
+print("The data type of num3 is:", type(num3))
+print("The data type of num4 is:", type(num4))
+print("The data type of check1 is:", type(check1))
+print("The data type of check2 is:", type(check2))
+
+print("Integer are Data type which are used to store whole numbers.")
+print("Float are Data type which are used to store decimal numbers.")
+print("Boolean are Data type which are used to store True or False values.")
+
+print("nonetype is a Data type which is used to store None value.")
+none_value = None
+print("The data type of none_value is:", type(none_value))
+
+print("Comments in Python are used to explain the code and make it more readable.")
+print("Single line comments are written using # symbol.")
+#This is a single line comment
+print("Multi line comments are written using triple quotes.")
+"""
+This is a multi-line comment
+line1
+line2
+"""
+
+print("Operators in Python are used to perform operations on variables and values.")
+print("Arithmetic operators are used to perform arithmetic operations on variables and values.")
+print("Comparison operators are used to compare two values and return a boolean value.")
+print("Logical operators are used to combine two or more boolean values and return a boolean value.")
+print("Assignment operators are used to assign values to variables.")
+
+print("Type Conversion in Python is used to convert one data type to another data type.")
+print("The int() function is used to convert a value to an integer data type.")
+print("The float() function is used to convert a value to a float data type.")
+print("The str() function is used to convert a value to a string data type.")
+print("The bool() function is used to convert a value to a boolean data type.")
+
+print("Lets try to convert the value of num1 to integer data type.")
+num1_int = int(num1)
+print("The value of num1 is:", num1, "and its data type is:", type(num1))
+print("The value of num1_int is:", num1_int, "and its data type is:", type(num1_int))
+
+print("Lets try to convert the value of num2 to float data type.")
+num2_float = float(num2)
+print("The value of num2 is:", num2, "and its data type is:", type(num2))
+print("The value of num2_float is:", num2_float, "and its data type is:", type(num2_float))
+
+print("Lets try to convert the value of age to string data type.")
+age_str = str(age)
+print("The value of age is:", age, "and its data type is:", type(age))
+print("The value of age_str is:", age_str, "and its data type is:", type(age_str))
+
+print("Lets try to convert the value of check1 to boolean data type.")
+check1_bool = bool(check1)
+print("The value of check1 is:", check1, "and its data type is:", type(check1))
+print("The value of check1_bool is:", check1_bool, "and its data type is:", type(check1_bool))
+
+print("Type Casting in Python is used to convert one data type to another data type.")
+print("The int() function is used to convert a value to an integer data type.")
+print("The float() function is used to convert a value to a float data type.")
+print("The str() function is used to convert a value to a string data type.")
+print("The bool() function is used to convert a value to a boolean data type.")
+
+print("Difference between Type Conversion and Type Casting is that Type Conversion is done automatically by Python whereas Type Casting is done manually by the programmer.")
+
+print("Inputs in Python are used to take input from the user.")
+print("The input() function is used to take input from the user.")
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+print("Hello", user_name, "you are", user_age, "years old.")
